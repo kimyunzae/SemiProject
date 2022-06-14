8a8c8ccc7bc7a9dd72ca2c3b4f45f00103721b71
kimyunzae <whkim1006@gmail.com>
2022-06-14 16:18:56 +0900

    first commit

---
+++ src/main/java/com/multi/TeaCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.multi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeaCoffeeApplication.class, args);
+	}
+
+}
