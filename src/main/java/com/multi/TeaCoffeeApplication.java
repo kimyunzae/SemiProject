5d73cd2314ba425dc9f664431f68e4890156069a
kimyunzae <whkim1006@gmail.com>
2022-06-15 13:20:44 +0900

    navbar-menus

--- src/main/java/com/multi/TeaCoffeeApplication.java
+++ src/main/java/com/multi/TeaCoffeeApplication.java
@@ -9,5 +9,7 @@ public class TeaCoffeeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TeaCoffeeApplication.class, args);
 	}
+	
+	
 
 }
---
+++ src/main/java/com/multi/controller/MainController.java
@@ -0,0 +1,30 @@
+package com.multi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/index")
+	public String index(Model m) {
+		return "index";
+	}
+
+	@RequestMapping("/about")
+	public String about(Model m) {
+		return "about";
+	}
+
+	@RequestMapping("/products")
+	public String products(Model m) {
+		return "products";
+	}
+
+	@RequestMapping("/store")
+	public String store(Model m) {
+		return "store";
+	}
+
+}
