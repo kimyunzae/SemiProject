a348dadf62d3e83be39b388ac8ab03114292cfa4
sangeun100 <qortkddms96@naver.com>
2022-06-16 13:41:35 +0900

    cate test

--- src/test/java/com/multi/cate/CateInsertTest.java
+++ src/test/java/com/multi/cate/CateInsertTest.java
@@ -1,24 +1,28 @@
-package com.multi.cust;
+package com.multi.cate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.multi.biz.CateBiz;
 import com.multi.biz.CustBiz;
+import com.multi.vo.CateVO;
+import com.multi.vo.CustVO;
 
 @SpringBootTest
-class CustDeleteTest {
+class CateInsertTest {
 	
 	@Autowired
-	CustBiz biz;
+	CateBiz biz;
 
 	@Test
 	void contextLoads() {
+		CateVO obj = new CateVO(12,"Espresso2",10);
 		try {
-			biz.remove("id05");
-			System.out.print("Delete OK");
+			biz.register(obj);
+			System.out.println("Registered OK");
+			System.out.println(obj);
 		} catch (Exception e) {
-			System.out.print("Delete FAIL");
 			e.printStackTrace();
 		}
 	}
--- src/test/java/com/multi/cate/CateSelectTest.java
+++ src/test/java/com/multi/cate/CateSelectTest.java
@@ -1,26 +1,31 @@
-package com.multi.cust;
+package com.multi.cate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.multi.biz.CateBiz;
 import com.multi.biz.CustBiz;
+import com.multi.vo.CateVO;
+import com.multi.vo.CustVO;
 
 @SpringBootTest
-class CustDeleteTest {
+class CateSelectTest {
 	
 	@Autowired
-	CustBiz biz;
-
+	CateBiz biz;
+	
 	@Test
 	void contextLoads() {
+		CateVO obj = null;
 		try {
-			biz.remove("id05");
-			System.out.print("Delete OK");
+			obj = biz.get(5);
+			System.out.println(obj);
 		} catch (Exception e) {
-			System.out.print("Delete FAIL");
 			e.printStackTrace();
 		}
 	}
 
 }
+
+
