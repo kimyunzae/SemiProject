1dee4cf459ee9dee20085009d9f96398c6a28e83
scl67ooo <evenif1458+sclgh@gmail.com>
2022-06-23 10:43:01 +0900

    profile edit

--- src/test/java/com/multi/cust/CustUpdateDefShipAddrTest.java
+++ src/test/java/com/multi/cust/CustUpdateDefShipAddrTest.java
@@ -1,28 +1,28 @@
-package com.multi.cust;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import com.multi.biz.CustBiz;
-import com.multi.vo.CustVO;
-
-@SpringBootTest
-class CustUpdateDefShipAddrTest {
-	
-	@Autowired
-	CustBiz biz;
-
-	@Test
-	void contextLoads() {
-		CustVO obj = new CustVO("id01",8);
-		try {
-			biz.modifyDefShipAddr(obj);
-			System.out.println("Update OK");
-			System.out.println(obj);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-}
+//package com.multi.cust;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import com.multi.biz.CustBiz;
+//import com.multi.vo.CustVO;
+//
+//@SpringBootTest
+//class CustUpdateDefShipAddrTest {
+//	
+//	@Autowired
+//	CustBiz biz;
+//
+//	@Test
+//	void contextLoads() {
+//		CustVO obj = new CustVO("id01",8);
+//		try {
+//			biz.modifyDefShipAddr(obj);
+//			System.out.println("Update OK");
+//			System.out.println(obj);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//
+//}
